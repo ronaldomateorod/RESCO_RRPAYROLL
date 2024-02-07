02b2621847464d52c4c31d9fda193375ac29a0e5
ronaldomateo <rm2018-0607@uce.edu.do>
2024-02-07 12:35:33 -0400

    Add the class program.cs in the main folder...

--- RESCO_RRPAYROLL/Program.cs
+++ RESCO_RRPAYROLL/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RESCO_RRPAYROLL.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RRPAYROLL_DBContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
